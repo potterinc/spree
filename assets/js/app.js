4f290ef0e2b851a8c800d1e35c4798e672056f9e
potterinc <harry_p30@yahoo.com>
2020-11-21 16:30:03 +0100

    FONTEND: Password reset

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,4 @@
 
-// Password Verification Asychronous Request 
-$('#VerifyAccount').click(function () {
-    validateInput('validateAccount');
-
-    //Sending asynchronous request
-    if (User.flag == true) {
-        $('#VerifyAccount').html('<img src="./images/dual-ring-loader.gif" width="32" />');
-        setTimeout(function () {
-            $.ajax({
-                url: '../config/auth.php',
-                dataType: User.returnType,
-                type: User.requestType[0],
-                data: {
-                    email: User.Email.val(),
-                    answer: User.Answer.val(),
-                    securityQuestion: User.Question.val()
-                },
-                success: function (asyncRequest) {
-                    User.Email.val(null);
-                    User.Answer.val(null);
-                    User.securityQuestion.val(null);
-
-                    if (asyncRequest.status == true) {
-                        $('#small-dialog2').html(User.ChangePassword.html());
-                        User.verifiedUserId.val(authResponse.userId);
-                    }
-                    else {
-
-                        $('#AccountVerificationStatus').html(authResponse.error);
-                        setTimeout(function () {
-                            $('#AccountVerificationStatus').fadeOut(1000);
-                        }, 5000);
-                        $("#AccountVerificationStatus").val(null).show();
-                    }
-                }
-            })
-        }, 3000);
-    }
-});
 
 
 // Password Reset Asychronous Request 
@@ -50,30 +11,28 @@ $('#ResetPassword').click(function () {
 
         if (User.confirmNewPassword.val() == User.Password.val()) {
 
-            setTimeout(function () {
-                $.ajax({
-                    url: '../config/auth.php',
-                    dataType: User.returnType,
-                    type: User.requestType[0],
-                    data: {
-                        password: User.Password.val(),
-                        userId: User.verifiedUserId.val()
-                    },
-                    success: function (asyncRequest) {
-                        User.Password.val(null);
-                        User.confirmNewPassword.val(null);
+            $.ajax({
+                url: '../config/auth.php',
+                dataType: User.returnType,
+                type: User.requestType[0],
+                data: {
+                    password: User.Password.val(),
+                    userId: User.verifiedUserId.val()
+                },
+                success: function (asyncRequest) {
+                    User.Password.val(null);
+                    User.confirmNewPassword.val(null);
 
-                        if (asyncRequest.Status == true) {
-                            $('#PasswordResetStatus').html(asyncRequest.Message);
-                            setTimeout(function () {
-                                $('#PasswordResetStatus').fadeOut(1000);
-                                $('#PasswordResetStatus').val('Redirecting...').show();
-                                location.href = '../index.html';
-                            }, 5000)
-                        }
+                    if (asyncRequest.Status == true) {
+                        $('#PasswordResetStatus').html(asyncRequest.Message);
+                        setTimeout(function () {
+                            $('#PasswordResetStatus').fadeOut(1000);
+                            $('#PasswordResetStatus').val('Redirecting...').show();
+                            location.href = '../index.html';
+                        }, 5000)
                     }
-                })
-            }, 3000)
+                }
+            })
         }
         else {
             $('#PasswordResetStatus').html('<span class="w3-animate-top w3-red w3-padding-8 w3-center">Password Does not match\nTry Agian.</span>')
@@ -87,7 +46,6 @@ $('#ResetPassword').click(function () {
 });
 
 var Auth = {
-
     // Form Validation
     formValidate: (inputArgs) => {
         let validInput = $('[' + inputArgs + ']');
@@ -102,76 +60,104 @@ var Auth = {
 
     // Login Algorithm
     login: () => {
-        $('#sign-in-button').html('Signing in...');
+        $.ajax({
+            url: '../config/auth.php',
+            type: User.XHR.POST,
+            dataType: User.JSONtype,
+            beforeSend: () => { $('#sign-in-button').html('Signing in...') },
+            data: {
+                email: User.email.loginEmail.val(),
+                password: User.password.loginPassword.val(),
+            },
+            success: function (asyncRequest) {
+                User.email.loginEmail.val(null);
+                User.password.loginPassword.val(null);
+                $('#login-status').html(asyncRequest);
 
-        setTimeout(() => {
-            $.ajax({
-                url: '../config/auth.php',
-                type: User.XHR.POST,
-                dataType: User.JSONtype,
-                beforeSend: ()=>{$(this).html('Please wait...')},
-                data: {
-                    email: User.email.loginEmail.val(),
-                    password: User.password.loginPassword.val(),
-                },
-                success: function (asyncRequest) {
-                    User.email.loginEmail.val(null);
-                    User.password.loginPassword.val(null);
-                    $('#login-status').html(asyncRequest);
+                setTimeout(() => {
+                    $('#loginStatus').fadeOut(1000);
+                }, 5000);
 
-                    setTimeout(() => {
-                        $('#loginStatus').fadeOut(1000);
-                    }, 5000);
-
-                    $('#loginStatus').val(null).show();
-                }
-            })
-        }, 3000);
+                $('#loginStatus').val(null).show();
+            }
+        })
     },
 
     // Sign Up
     signUp: () => {
+        if (User.password.signUpConfirmPassword === User.password.signUpPassword) {
+            $.ajax({
+                url: '../config/auth.php',
+                type: User.XHR.POST,
+                dataType: User.JSONtype,
+                beforeSend: () => { $(this).html('Please wait...') },
+                data: {
+                    fullName: User.fullName.val(),
+                    email: User.email.signUpEmail.val(),
+                    telephone: User.telephone.val(),
+                    securityQuestion: User.question.signUpQuestion.val(),
+                    answer: User.answer.signUpAnswer.val(),
+                    password: User.password.signUpPassword.val(),
+                    dateOfRegistration: User.getToday()
+                },
+                success: (asyncRequest) => {
+                    User.fullName.val(null);
+                    User.email.signUpEmail.val(null);
+                    User.telephone.val(null);
+                    User.question.signUpQuestion.val(null);
+                    User.answer.signUpAnswer.val(null);
+                    User.password.signUpPassword.val(null);
+                    $('#SignUpNotification').html(asyncRequest);
+                    setTimeout(() => {
+                        $('#notification').fadeOut(1000);
+                        $('#notification').val(null).show();
+                    }, 5000)
+                }
+            })
+        }
+        else {
+            setTimeout(() => {
+                $('#notification').html('Password Does not match: Try again')
+                $('#notification').fadeOut(1000)
+                $('#notification').val(null).show();
+                return false;
+            }, 5000)
+        }
+    },
 
-        setTimeout(() => {
-            if (User.password.signUpConfirmPassword === User.password.signUpPassword) {
+    // Password verification
+    verifyPassword: () => {
+        $.ajax({
+            url: '../config/auth.php',
+            dataType: User.JSONtype,
+            type: User.XHR.POST,
+            data: {
+                email: User.email.resetEmail.val(),
+                answer: User.answer.resetAnswer.val(),
+                securityQuestion: User.question.resetQuestion.val()
+            },
+            beforeSend: () => {
+                $('#verify').html('Please wait...')
+            },
+            success: function (asyncRequest) {
+                User.email.resetEmail.val(null);
+                User.answer.resetAnswer.val(null);
+                User.question.resetQuestion.val(null);
 
-                $.ajax({
-                    url: '../config/auth.php',
-                    type: User.XHR.POST,
-                    dataType: User.JSONtype,
-                    beforeSend: ()=>{$(this).html('Please wait...')},
-                    data: {
-                        fullName: User.fullName.val(),
-                        email: User.email.signUpEmail.val(),
-                        telephone: User.telephone.val(),
-                        securityQuestion: User.question.signUpQuestion.val(),
-                        answer: User.answer.signUpAnswer.val(),
-                        password: User.password.signUpPassword.val(),
-                        dateOfRegistration: User.getToday()
-                    },
-                    success: (asyncRequest) => {
-                        User.fullName.val(null);
-                        User.email.signUpEmail.val(null);
-                        User.telephone.val(null);
-                        User.question.signUpQuestion.val(null);
-                        User.answer.signUpAnswer.val(null);
-                        User.password.signUpPassword.val(null);
-                        $('#SignUpNotification').html(asyncRequest);
-                        setTimeout(() => {
-                            $('#notification').fadeOut(1000);
-                            $('#notification').val(null).show();
-                        }, 5000)
-                    }
-                })
+                if (asyncRequest.status == true) {
+                    $('#small-dialog2').html(User.ChangePassword.html());
+                    User.verifiedUserId.val(authResponse.userId);
+                }
+                else {
+
+                    $('#AccountVerificationStatus').html(authResponse.error);
+                    setTimeout(function () {
+                        $('#AccountVerificationStatus').fadeOut(1000);
+                    }, 5000);
+                    $("#AccountVerificationStatus").val(null).show();
+                }
             }
-            else
-                setTimeout(() => {
-                    $('#notification').html('Password Does not match: Try again')
-                    $('#notification').fadeOut(1000)
-                    $('#notification').val(null).show();
-                    return false;
-                }, 5000)
-        }, 3000)
+        })
     }
 }
 
@@ -183,6 +169,7 @@ var User = {
     email: {
         signUpEmail: $('#sign-up-email'),
         loginEmail: $('#login-email'),
+        resetEmail: $('#reset-email'),
     },
     password: {
         passwordResetSection: '',
